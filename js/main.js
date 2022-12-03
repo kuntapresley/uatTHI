5e3dbf4b2e6b53cb8cd380fff2ba494164b41952
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2022-12-03 12:39:45 +0100

    post

--- js/main.js
+++ js/main.js
@@ -566,6 +566,28 @@ faqFunction();
 
 
 
+function ourTeamAccordion() {
+
+    var acc = document.getElementsByClassName("c_about_our_team_accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("ourTeamIcons");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+    });
+    }
+
+}
+
+ourTeamAccordion();
+
+
 
 
 
